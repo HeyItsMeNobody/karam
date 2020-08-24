408a0f40e0f23e3d058fa51ad9095fe432fc1117
dyon <dyon@dyonb.nl>
2020-08-24 19:51:46 +0200

    Remove massive comment

--- src/main/java/nl/dyonb/karam/mixin/EntityMixin.java
+++ src/main/java/nl/dyonb/karam/mixin/EntityMixin.java
@@ -33,53 +33,6 @@ public abstract class EntityMixin {
         // Check if the entity is sneaking
         if (sneaking == true) {
             ElevatorBlock.moveInDirection(Direction.DOWN, (Entity) (Object) this);
-//            // Get the block position
-//            BlockPos pos = getBlockPos();
-//            // Store the original value for later usage
-//            BlockPos originalPos = getBlockPos();
-//            // Integer for the amount of full cubes passed
-//            int fullCubeBlocksPassed = 0;
-//            // Get the block one down
-//            Block block = world.getBlockState(pos.down()).getBlock();
-//            // Check if the block is an elevator
-//            if (block instanceof ElevatorBlock && world.getBlockState(pos).getCollisionShape(world, pos).isEmpty()) {
-//                // Count how many blocks have gone down
-//                int totalBlocksTraveled = 1;
-//                // Set pos one lower than the ElevatorBlock
-//                pos = pos.down();
-//                // While the Y level isn't 0
-//                while(pos.getY() > 0) {
-//                    // Check if the block downwards is an elevator block
-//                    if (world.getBlockState(pos.down()).getBlock().equals(block)) {
-//                        // Teleport to that elevator
-//                        teleport(this.pos.x, pos.down().getY() + 1.15, this.pos.z);
-//                        // Play the ender chest open sound
-//                        world.playSound(null, pos, SoundEvents.BLOCK_ENDER_CHEST_OPEN, SoundCategory.BLOCKS, 0.5F, world.random.nextFloat() * 0.15F + 0.6F);
-//                        break;
-//                    } else if (totalBlocksTraveled == KaramConfig.CONFIG.maxBlocksBetweenElevator) {
-//                        // Break if max blocks have been passed and play the ender eye death sound
-//                        world.playSound(null, originalPos, SoundEvents.ENTITY_ENDER_EYE_DEATH, SoundCategory.BLOCKS, 0.5F, world.random.nextFloat() * 0.15F + 0.6F);
-//                        break;
-//                    } else if (fullCubeBlocksPassed == KaramConfig.CONFIG.maxSolidBlocksBetweenElevator) {
-//                        // Break if max solid blocks have been passed and play the ender eye death sound
-//                        world.playSound(null, originalPos, SoundEvents.ENTITY_ENDER_EYE_DEATH, SoundCategory.BLOCKS, 0.5F, world.random.nextFloat() * 0.15F + 0.6F);
-//                        break;
-//                    } else {
-//                        // If that block isn't an elevator block go one more down
-//                        pos = pos.down();
-//                        // Increase the total blocks traveled
-//                        totalBlocksTraveled++;
-//                        // If the block at the pos is a full cube, Add to the full cube counter.
-//                        if (world.getBlockState(pos).isFullCube(world, pos)) {
-//                            fullCubeBlocksPassed++;
-//                        }
-//                    }
-//                }
-//                if (pos.getY() == 0) {
-//                    // Play the ender eye death sound if the world limit has been reached
-//                    world.playSound(null, originalPos, SoundEvents.ENTITY_ENDER_EYE_DEATH, SoundCategory.BLOCKS, 0.5F, world.random.nextFloat() * 0.15F + 0.6F);
-//                }
-//            }
         }
     }
 
